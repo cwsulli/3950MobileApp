3e38cad5290ac97a4ea3ca3c9b78007bcf728f2e
cwsulli <casey.sullivan1@bobcats.gcsu.edu>
2016-06-15 21:19:00 -0400

    SOAP/JSONcommit

--- Soap/platforms/browser/www/js/index.js
+++ Soap/platforms/browser/www/js/index.js
@@ -1,59 +1,61 @@
 function init() {
-    document.getElementById("btnJoke").addEventListener('click', getJoke, false);
-    document.getElementById("btnQuery").addEventListener('click', getNewJoke, false);
-    document.getElementById("btnZip").addEventListener('click', getWeather, false);
+    //document.getElementById("btnJoke").addEventListener('click', getJoke, false);
+    //document.getElementById("btnQuery").addEventListener('click', getNewJoke, false);
+    //document.getElementById("btnZip").addEventListener('click', getWeather, false);
     //document.getElementById("zip").addEventListener('change', getWeather, false);
 }
 
 window.onload = function () {
-    document.addEventListener("deviceready", init, false);
+        document.addEventListener("deviceready", init, false);
 
-    init();
-}
+        getTrainTable();
 
-function getJoke() {
-    $.ajax({
-        type: "GET",
-        url: "http://api.icndb.com/jokes/random/",
-        dataType: "text",
-        sucess: function (result) {
-            showJoke(result);
-        }
-    });
-}
+        init();
+    }
+    /*
+    function getJoke() {
+        $.ajax({
+            type: "GET",
+            url: "http://api.icndb.com/jokes/random/",
+            dataType: "text",
+            sucess: function (result) {
+                showJoke(result);
+            }
+        });
+    }
 
-function showJoke(result) {
-    console.log("showJoke: " + result);
+    function showJoke(result) {
+        console.log("showJoke: " + result);
 
-    var json = JQuery.parseJSON(result);
-    document.getElementById('resultJoke').innerHTML = json.value.joke;
-}
+        var json = JQuery.parseJSON(result);
+        document.getElementById('resultJoke').innerHTML = json.value.joke;
+    }
 
-function getNewJoke() {
-    var fName = document.getElementById('fName').value;
-    var lName = document.getElementById('lName').value;
+    function getNewJoke() {
+        var fName = document.getElementById('fName').value;
+        var lName = document.getElementById('lName').value;
 
-    $.ajax({
-        type: "GET",
-        url: "http://api.icndb.com/jokes/random/?firstName=" + fName + "&lastName=" + lName,
-        dataTpe: "text",
-        success: function (result) {
-            showJokeQuery(result);
-        }
-    });
-}
+        $.ajax({
+            type: "GET",
+            url: "http://api.icndb.com/jokes/random/?firstName=" + fName + "&lastName=" + lName,
+            dataTpe: "text",
+            success: function (result) {
+                showJokeQuery(result);
+            }
+        });
+    }
 
-function showJokeQuery(result) {
-    var json = jQuery.parseJSON(result);
-    document.getElementById('resultQuery').innerHTML = json.value.joke;
-}
-
+    function showJokeQuery(result) {
+        var json = jQuery.parseJSON(result);
+        document.getElementById('resultQuery').innerHTML = json.value.joke;
+    }
+    
 function getWeather() {
     var zip = document.getElementById('zip').value;
 
     $.ajax({
         type: "GET",
-        url: "http://wsf.cdyne.com/WeatherWS/Weather.asmx/GetCityWeatherByZIP=" + zip,
+        url: "http://wsf.cdyne.com/WeatherWS/Weather.asmx/GetCityWeatherByZIP?Zip=" + zip,
         dataType: "text",
         success: function (xml) {
             showWeather(xml);
@@ -72,4 +74,45 @@ function showWeather(xml) {
     output += "Temperature: " + temperature + "<br />";
     output += "Description: " + description + "<br />";
     document.getElementById("resultWeather").innerHTML = output;
+}
+*/
+function getTrainTable() {
+    $.ajax({
+        type: "GET",
+        url: "http://www3.septa.org/hackathon/Arrivals/90404/10",
+        dataType: "text",
+        success: function (text) {
+            showTrainTable(text);
+        }
+    });
+}
+
+function showTrainTable(result) {
+    console.log("showTrain function");
+
+    var data = jQuery.parseJSON(result);
+    console.log("data is set");
+
+    var arr = data[Object.keys(data)];
+    console.log("array initiated");
+
+    var northbound = arr[0].Northbound;
+    var southbound = arr[1].Southbound;
+
+    console.log("north and south are set");
+
+    var output = "<div class='table'> <h1>Northbound</h1><br> <div class='row'>";
+    for (i = 0; i < northbound.length; i++) {
+        output += "<div class='cell1 border block'>" + northbound[i].train_id + ": " + northbound[i].destination + "</div>";
+    }
+
+    output += "</div><h1>Southbound</h1><br> <div class='row'>";
+    for (i = 0; i < southbound.length; i++) {
+        output += "<div class='cell2 border block'>";
+        output += southbound[i].train_id + ": " + southbound[i].destination + "</div>";
+    }
+
+    output += "</div></div>"
+    document.getElementById("train").innerHTML = output;
+    console.log(output);
 }
--- Soap/www/js/index.js
+++ Soap/www/js/index.js
@@ -1,55 +1,55 @@
 function init() {
-    document.getElementById("btnJoke").addEventListener('click', getJoke, false);
-    document.getElementById("btnQuery").addEventListener('click', getNewJoke, false);
-    document.getElementById("btnZip").addEventListener('click', getWeather, false);
+    //document.getElementById("btnJoke").addEventListener('click', getJoke, false);
+    //document.getElementById("btnQuery").addEventListener('click', getNewJoke, false);
+    //document.getElementById("btnZip").addEventListener('click', getWeather, false);
     //document.getElementById("zip").addEventListener('change', getWeather, false);
 }
 
 window.onload = function () {
-    document.addEventListener("deviceready", init, false);
+        document.addEventListener("deviceready", init, false);
 
-    getTrainTable();
+        getTrainTable();
 
-    init();
-}
+        init();
+    }
+    /*
+    function getJoke() {
+        $.ajax({
+            type: "GET",
+            url: "http://api.icndb.com/jokes/random/",
+            dataType: "text",
+            sucess: function (result) {
+                showJoke(result);
+            }
+        });
+    }
 
-function getJoke() {
-    $.ajax({
-        type: "GET",
-        url: "http://api.icndb.com/jokes/random/",
-        dataType: "text",
-        sucess: function (result) {
-            showJoke(result);
-        }
-    });
-}
+    function showJoke(result) {
+        console.log("showJoke: " + result);
 
-function showJoke(result) {
-    console.log("showJoke: " + result);
+        var json = JQuery.parseJSON(result);
+        document.getElementById('resultJoke').innerHTML = json.value.joke;
+    }
 
-    var json = JQuery.parseJSON(result);
-    document.getElementById('resultJoke').innerHTML = json.value.joke;
-}
+    function getNewJoke() {
+        var fName = document.getElementById('fName').value;
+        var lName = document.getElementById('lName').value;
 
-function getNewJoke() {
-    var fName = document.getElementById('fName').value;
-    var lName = document.getElementById('lName').value;
+        $.ajax({
+            type: "GET",
+            url: "http://api.icndb.com/jokes/random/?firstName=" + fName + "&lastName=" + lName,
+            dataTpe: "text",
+            success: function (result) {
+                showJokeQuery(result);
+            }
+        });
+    }
 
-    $.ajax({
-        type: "GET",
-        url: "http://api.icndb.com/jokes/random/?firstName=" + fName + "&lastName=" + lName,
-        dataTpe: "text",
-        success: function (result) {
-            showJokeQuery(result);
-        }
-    });
-}
-
-function showJokeQuery(result) {
-    var json = jQuery.parseJSON(result);
-    document.getElementById('resultQuery').innerHTML = json.value.joke;
-}
-
+    function showJokeQuery(result) {
+        var json = jQuery.parseJSON(result);
+        document.getElementById('resultQuery').innerHTML = json.value.joke;
+    }
+    
 function getWeather() {
     var zip = document.getElementById('zip').value;
 
@@ -75,34 +75,44 @@ function showWeather(xml) {
     output += "Description: " + description + "<br />";
     document.getElementById("resultWeather").innerHTML = output;
 }
-
+*/
 function getTrainTable() {
     $.ajax({
         type: "GET",
-        url: "https://www3.septa.org/hackathon/Arrivals/90404/10",
-        dataTpe: "text",
-        success: function (xml) {
-            showJokeQuery(xml);
+        url: "http://www3.septa.org/hackathon/Arrivals/90404/10",
+        dataType: "text",
+        success: function (text) {
+            showTrainTable(text);
         }
     });
 }
 
 function showTrainTable(result) {
-    console.log("showTrain function")
+    console.log("showTrain function");
+
     var data = jQuery.parseJSON(result);
+    console.log("data is set");
 
     var arr = data[Object.keys(data)];
+    console.log("array initiated");
+
     var northbound = arr[0].Northbound;
     var southbound = arr[1].Southbound;
 
-    var output = "Northbound<br>";
-    for (i = 0; i < northbound.length; i++)
-        output += northbound[i].train_id + ": " + northbound[i].destination + "<br />";
+    console.log("north and south are set");
 
-    output += "Southbound<br>";
-    for (i = 0; i < southbound.length; i++)
-        output += southbound[i].train_id + ": " + southbound[i].destination + "<br />";
+    var output = "<div class='table'> <h1>Northbound:</h1> <div class='row'>";
+    for (i = 0; i < northbound.length; i++) {
+        output += "<div class='cell1 border block'>" + northbound[i].train_id + ": " + northbound[i].destination + "</div>";
+    }
 
+    output += "</div><h1>Southbound:</h1> <div class='row'>";
+    for (i = 0; i < southbound.length; i++) {
+        output += "<div class='cell2 border block'>";
+        output += southbound[i].train_id + ": " + southbound[i].destination + "</div>";
+    }
+
+    output += "</div></div>"
     document.getElementById("train").innerHTML = output;
     console.log(output);
 }
